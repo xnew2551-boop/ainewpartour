22c652772d131a487d82ce3b5ba474918a990c36
xnew2551-boop <xnew2551@gmail.com>
2026-05-20 23:21:20 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
+module.exports = {
   content: ['./app/**/*.{js,jsx}', './components/**/*.{js,jsx}', './lib/**/*.{js,jsx}'],
   theme: {
     extend: {
@@ -16,5 +16,3 @@ const config = {
   },
   plugins: []
 };
-
-export default config;
